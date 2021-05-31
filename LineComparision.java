1fa68bf269f01eace1241ef875bad3e29ebbcd81
SgOrigin999 <you@example.com>
2021-05-31 19:05:25 +0530

    Computation of Line Comparision

---
+++ LineComparision.java
@@ -0,0 +1,9 @@
+package com.bridgelab.linecomparision.day5;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparision Computation");
+		
+	}
+}
